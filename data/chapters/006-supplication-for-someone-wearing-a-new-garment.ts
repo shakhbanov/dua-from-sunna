bbd97553f7c66119df9f0da8c242ffcdc941337b
shakhbanov <shakhbanov_zurab@icloud.com>
2026-04-20 06:42:59 +0000

    Add timestamps for duas 6-1 and 6-2

--- data/chapters/006-supplication-for-someone-wearing-a-new-garment.ts
+++ data/chapters/006-supplication-for-someone-wearing-a-new-garment.ts
@@ -12,10 +12,10 @@ export const CHAPTER_006: ChapterData = {
           en: "May Allah the Exalted give you a replacement when [the garment] wears out."
         },
         sync: [
-          { text: "تُبْلِي", trans: { ru: "[Когда] ты износишь", en: "[When] you wear it out" }, start: 0, end: 0 },
-          { text: "وَيُخْلِفُ", trans: { ru: "да возместит", en: "may replace it" }, start: 0, end: 0 },
-          { text: "اللَّهُ", trans: { ru: "Аллах", en: "Allah" }, start: 0, end: 0 },
-          { text: "تَعَالَىٰ", trans: { ru: "Всевышний", en: "the Exalted" }, start: 0, end: 0 }
+          { text: "تُبْلِي", trans: { ru: "[Когда] ты износишь", en: "[When] you wear it out" }, start: 0.261, end: 1.123 },
+          { text: "وَيُخْلِفُ", trans: { ru: "да возместит", en: "may replace it" }, start: 1.203, end: 2.025 },
+          { text: "اللَّهُ", trans: { ru: "Аллах", en: "Allah" }, start: 2.065, end: 2.300 },
+          { text: "تَعَالَىٰ", trans: { ru: "Всевышний", en: "the Exalted" }, start: 2.340, end: 3.920 }
         ],
         source: { ru: "Абу Дауд 4020", en: "Abu Dawud 4020" }
       },
@@ -27,12 +27,12 @@ export const CHAPTER_006: ChapterData = {
           en: "Wear it out anew, live in praise, and die a martyr."
         },
         sync: [
-          { text: "الْبَسْ", trans: { ru: "Носи", en: "Wear" }, start: 0, end: 0 },
-          { text: "جَدِيدًا", trans: { ru: "новое", en: "new [clothing]" }, start: 0, end: 0 },
-          { text: "وَعِشْ", trans: { ru: "и живи", en: "and live" }, start: 0, end: 0 },
-          { text: "حَمِيدًا", trans: { ru: "достойно", en: "praiseworthy" }, start: 0, end: 0 },
-          { text: "وَمُتْ", trans: { ru: "и умри", en: "and die" }, start: 0, end: 0 },
-          { text: "شَهِيدًا", trans: { ru: "шахидом", en: "a martyr" }, start: 0, end: 0 }
+          { text: "الْبَسْ", trans: { ru: "Носи", en: "Wear" }, start: 0.240, end: 0.901 },
+          { text: "جَدِيدًا", trans: { ru: "новое", en: "new [clothing]" }, start: 0.962, end: 2.584 },
+          { text: "وَعِشْ", trans: { ru: "и живи", en: "and live" }, start: 2.664, end: 3.165 },
+          { text: "حَمِيدًا", trans: { ru: "достойно", en: "praiseworthy" }, start: 3.285, end: 4.868 },
+          { text: "وَمُتْ", trans: { ru: "и умри", en: "and die" }, start: 4.948, end: 5.469 },
+          { text: "شَهِيدًا", trans: { ru: "шахидом", en: "a martyr" }, start: 5.509, end: 6.170 }
         ],
         source: { ru: "Ибн Маджа 3558; Сильсиля ас-сахиха 352", en: "Ibn Majah 3558; Silsilah as-Sahihah 352" }
       }
