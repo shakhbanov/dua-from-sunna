3cfd6ec1b7fccc2e84e549c2bcab09f77ae22809
shakhbanov <shakhbanov_zurab@icloud.com>
2026-04-20 06:06:33 +0000

    Add timestamps for dua 4-1 via forced alignment

--- data/chapters/004-supplication-when-wearing-a-garment.ts
+++ data/chapters/004-supplication-when-wearing-a-garment.ts
@@ -12,19 +12,19 @@ export const CHAPTER_004: ChapterData = {
           en: "Praise is to Allah who clothed me with this garment and provided it for me, though on my own I had neither power nor strength."
         },
         sync: [
-          { text: "الْحَمْدُ", trans: { ru: "Хвала", en: "Praise" }, start: 0, end: 0 },
-          { text: "لِلَّهِ", trans: { ru: "Аллаху", en: "to Allah" }, start: 0, end: 0 },
-          { text: "الَّذِي", trans: { ru: "Который", en: "Who" }, start: 0, end: 0 },
-          { text: "كَسَانِي", trans: { ru: "одел меня", en: "clothed me" }, start: 0, end: 0 },
-          { text: "هَٰذَا", trans: { ru: "в эту", en: "with this" }, start: 0, end: 0 },
-          { text: "الثَّوْبَ", trans: { ru: "одежду", en: "garment" }, start: 0, end: 0 },
-          { text: "وَرَزَقَنِيهِ", trans: { ru: "и наделил меня ею", en: "and provided me with it" }, start: 0, end: 0 },
-          { text: "مِنْ", trans: { ru: "без", en: "without" }, start: 0, end: 0 },
-          { text: "غَيْرِ", trans: { ru: "[всякого]", en: "any" }, start: 0, end: 0 },
-          { text: "حَوْلٍ", trans: { ru: "действия", en: "effort" }, start: 0, end: 0 },
-          { text: "مِنِّي", trans: { ru: "с моей стороны", en: "on my part" }, start: 0, end: 0 },
-          { text: "وَلَا", trans: { ru: "и без", en: "nor" }, start: 0, end: 0 },
-          { text: "قُوَّةٍ", trans: { ru: "[моей] силы", en: "[any] strength" }, start: 0, end: 0 }
+          { text: "الْحَمْدُ", trans: { ru: "Хвала", en: "Praise" }, start: 0.520, end: 1.621 },
+          { text: "لِلَّهِ", trans: { ru: "Аллаху", en: "to Allah" }, start: 1.681, end: 2.762 },
+          { text: "الَّذِي", trans: { ru: "Который", en: "Who" }, start: 2.782, end: 3.743 },
+          { text: "كَسَانِي", trans: { ru: "одел меня", en: "clothed me" }, start: 3.863, end: 5.064 },
+          { text: "هَٰذَا", trans: { ru: "в эту", en: "with this" }, start: 5.184, end: 5.784 },
+          { text: "الثَّوْبَ", trans: { ru: "одежду", en: "garment" }, start: 5.824, end: 6.765 },
+          { text: "وَرَزَقَنِيهِ", trans: { ru: "и наделил меня ею", en: "and provided me with it" }, start: 6.865, end: 8.566 },
+          { text: "مِنْ", trans: { ru: "без", en: "without" }, start: 8.626, end: 9.027 },
+          { text: "غَيْرِ", trans: { ru: "[всякого]", en: "any" }, start: 9.127, end: 9.747 },
+          { text: "حَوْلٍ", trans: { ru: "действия", en: "effort" }, start: 9.907, end: 10.628 },
+          { text: "مِنِّي", trans: { ru: "с моей стороны", en: "on my part" }, start: 11.228, end: 12.509 },
+          { text: "وَلَا", trans: { ru: "и без", en: "nor" }, start: 12.609, end: 13.150 },
+          { text: "قُوَّةٍ", trans: { ru: "[моей] силы", en: "[any] strength" }, start: 13.370, end: 13.910 }
         ],
         source: { ru: "Абу Дауд 4023; Сахих аль-калим 188", en: "Abu Dawud 4023; Sahih al-Kalim 188" }
       }
