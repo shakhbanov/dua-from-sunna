331e994465588063936e1d383bbf0f9c53e3f448
shakhbanov <shakhbanov_zurab@icloud.com>
2026-04-21 07:14:29 +0000

    Add timestamps for chapter 132 (5 duas)

--- data/chapters/132-excellence-of-remembrance-tasbih-tahmid-tahlil-takbir.ts
+++ data/chapters/132-excellence-of-remembrance-tasbih-tahmid-tahlil-takbir.ts
@@ -16,9 +16,9 @@ export const CHAPTER_132: ChapterData = {
           en: "Glory be to Allah and praise be to Him."
         },
         sync: [
-          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0, end: 0 },
-          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0, end: 0 },
-          { text: "وَبِحَمْدِهِ", trans: { ru: "и хвала Ему", en: "and praise be to Him" }, start: 0, end: 0 }
+          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0.341, end: 0.980 },
+          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0.980, end: 1.280 },
+          { text: "وَبِحَمْدِهِ", trans: { ru: "и хвала Ему", en: "and praise be to Him" }, start: 1.280, end: 2.520 }
         ],
         source: { ru: "аль-Бухари 6405; Муслим 2691", en: "al-Bukhari 6405; Muslim 2691" }
       },
@@ -34,23 +34,23 @@ export const CHAPTER_132: ChapterData = {
           en: "There is no god but Allah alone, with no partner. To Him belongs the dominion and to Him is all praise, and He is Powerful over everything."
         },
         sync: [
-          { text: "لَا", trans: { ru: "Нет", en: "(There is) no" }, start: 0, end: 0 },
-          { text: "إِلَٰهَ", trans: { ru: "бога", en: "god" }, start: 0, end: 0 },
-          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 0, end: 0 },
-          { text: "اللَّهُ", trans: { ru: "Аллаха", en: "Allah" }, start: 0, end: 0 },
-          { text: "وَحْدَهُ", trans: { ru: "одного Его", en: "alone" }, start: 0, end: 0 },
-          { text: "لَا", trans: { ru: "нет", en: "no" }, start: 0, end: 0 },
-          { text: "شَرِيكَ", trans: { ru: "сотоварища", en: "partner" }, start: 0, end: 0 },
-          { text: "لَهُ", trans: { ru: "у Него", en: "for Him" }, start: 0, end: 0 },
-          { text: "لَهُ", trans: { ru: "Ему принадлежит", en: "to Him (belongs)" }, start: 0, end: 0 },
-          { text: "الْمُلْكُ", trans: { ru: "владычество", en: "the dominion" }, start: 0, end: 0 },
-          { text: "وَلَهُ", trans: { ru: "и Ему", en: "and to Him" }, start: 0, end: 0 },
-          { text: "الْحَمْدُ", trans: { ru: "хвала", en: "(is) the praise" }, start: 0, end: 0 },
-          { text: "وَهُوَ", trans: { ru: "и Он", en: "and He" }, start: 0, end: 0 },
-          { text: "عَلَىٰ", trans: { ru: "над", en: "over" }, start: 0, end: 0 },
-          { text: "كُلِّ", trans: { ru: "всякой", en: "every" }, start: 0, end: 0 },
-          { text: "شَيْءٍ", trans: { ru: "вещью", en: "thing" }, start: 0, end: 0 },
-          { text: "قَدِيرٌ", trans: { ru: "мощен", en: "(is) All-Powerful" }, start: 0, end: 0 }
+          { text: "لَا", trans: { ru: "Нет", en: "(There is) no" }, start: 0.360, end: 0.641 },
+          { text: "إِلَٰهَ", trans: { ru: "бога", en: "god" }, start: 0.721, end: 1.261 },
+          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 1.321, end: 1.702 },
+          { text: "اللَّهُ", trans: { ru: "Аллаха", en: "Allah" }, start: 1.742, end: 2.282 },
+          { text: "وَحْدَهُ", trans: { ru: "одного Его", en: "alone" }, start: 2.342, end: 2.983 },
+          { text: "لَا", trans: { ru: "нет", en: "no" }, start: 3.063, end: 3.324 },
+          { text: "شَرِيكَ", trans: { ru: "сотоварища", en: "partner" }, start: 3.444, end: 4.004 },
+          { text: "لَهُ", trans: { ru: "у Него", en: "for Him" }, start: 4.064, end: 4.845 },
+          { text: "لَهُ", trans: { ru: "Ему принадлежит", en: "to Him (belongs)" }, start: 5.005, end: 5.306 },
+          { text: "الْمُلْكُ", trans: { ru: "владычество", en: "the dominion" }, start: 5.346, end: 5.986 },
+          { text: "وَلَهُ", trans: { ru: "и Ему", en: "and to Him" }, start: 6.046, end: 6.447 },
+          { text: "الْحَمْدُ", trans: { ru: "хвала", en: "(is) the praise" }, start: 6.487, end: 7.148 },
+          { text: "وَهُوَ", trans: { ru: "и Он", en: "and He" }, start: 7.188, end: 7.628 },
+          { text: "عَلَىٰ", trans: { ru: "над", en: "over" }, start: 7.768, end: 8.109 },
+          { text: "كُلِّ", trans: { ru: "всякой", en: "every" }, start: 8.269, end: 8.649 },
+          { text: "شَيْءٍ", trans: { ru: "вещью", en: "thing" }, start: 8.769, end: 9.370 },
+          { text: "قَدِيرٌ", trans: { ru: "мощен", en: "(is) All-Powerful" }, start: 9.510, end: 10.031 }
         ],
         source: { ru: "аль-Бухари 3293; Муслим 2691", en: "al-Bukhari 3293; Muslim 2691" }
       },
@@ -66,12 +66,12 @@ export const CHAPTER_132: ChapterData = {
           en: "Glory be to Allah and praise be to Him; glory be to Allah, the Great."
         },
         sync: [
-          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0, end: 0 },
-          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0, end: 0 },
-          { text: "وَبِحَمْدِهِ", trans: { ru: "и хвала Ему", en: "and praise be to Him" }, start: 0, end: 0 },
-          { text: "سُبْحَانَ", trans: { ru: "пречист", en: "glory be to" }, start: 0, end: 0 },
-          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0, end: 0 },
-          { text: "الْعَظِيمِ", trans: { ru: "Великий", en: "the Great" }, start: 0, end: 0 }
+          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0.361, end: 0.942 },
+          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0.982, end: 1.523 },
+          { text: "وَبِحَمْدِهِ", trans: { ru: "и хвала Ему", en: "and praise be to Him" }, start: 1.603, end: 3.246 },
+          { text: "سُبْحَانَ", trans: { ru: "пречист", en: "glory be to" }, start: 3.367, end: 4.028 },
+          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 4.048, end: 4.629 },
+          { text: "الْعَظِيمِ", trans: { ru: "Великий", en: "the Great" }, start: 4.669, end: 5.310 }
         ],
         source: { ru: "аль-Бухари 6406; Муслим 2694", en: "al-Bukhari 6406; Muslim 2694" }
       },
@@ -87,16 +87,16 @@ export const CHAPTER_132: ChapterData = {
           en: "Glory be to Allah; praise be to Allah; there is no god but Allah; Allah is the Greatest."
         },
         sync: [
-          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0, end: 0 },
-          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 0, end: 0 },
-          { text: "وَالْحَمْدُ", trans: { ru: "и хвала", en: "and praise" }, start: 0, end: 0 },
-          { text: "لِلَّهِ", trans: { ru: "Аллаху", en: "(be) to Allah" }, start: 0, end: 0 },
-          { text: "وَلَا", trans: { ru: "и нет", en: "and (there is) no" }, start: 0, end: 0 },
-          { text: "إِلَٰهَ", trans: { ru: "бога", en: "god" }, start: 0, end: 0 },
-          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 0, end: 0 },
-          { text: "اللَّهُ", trans: { ru: "Аллаха", en: "Allah" }, start: 0, end: 0 },
-          { text: "وَاللَّهُ", trans: { ru: "и Аллах", en: "and Allah" }, start: 0, end: 0 },
-          { text: "أَكْبَرُ", trans: { ru: "велик", en: "(is) greatest" }, start: 0, end: 0 }
+          { text: "سُبْحَانَ", trans: { ru: "Пречист", en: "Glory be to" }, start: 0.280, end: 0.961 },
+          { text: "اللَّهِ", trans: { ru: "Аллах", en: "Allah" }, start: 1.001, end: 1.602 },
+          { text: "وَالْحَمْدُ", trans: { ru: "и хвала", en: "and praise" }, start: 1.682, end: 2.403 },
+          { text: "لِلَّهِ", trans: { ru: "Аллаху", en: "(be) to Allah" }, start: 2.463, end: 3.244 },
+          { text: "وَلَا", trans: { ru: "и нет", en: "and (there is) no" }, start: 3.305, end: 4.206 },
+          { text: "إِلَٰهَ", trans: { ru: "бога", en: "god" }, start: 4.266, end: 4.887 },
+          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 4.947, end: 5.407 },
+          { text: "اللَّهُ", trans: { ru: "Аллаха", en: "Allah" }, start: 5.448, end: 6.169 },
+          { text: "وَاللَّهُ", trans: { ru: "и Аллах", en: "and Allah" }, start: 6.229, end: 7.030 },
+          { text: "أَكْبَرُ", trans: { ru: "велик", en: "(is) greatest" }, start: 7.090, end: 7.530 }
         ],
         source: { ru: "Муслим 2137", en: "Muslim 2137" }
       },
@@ -112,12 +112,12 @@ export const CHAPTER_132: ChapterData = {
           en: "There is no might and no power except with Allah."
         },
         sync: [
-          { text: "لَا", trans: { ru: "Нет", en: "(There is) no" }, start: 0, end: 0 },
-          { text: "حَوْلَ", trans: { ru: "силы", en: "might" }, start: 0, end: 0 },
-          { text: "وَلَا", trans: { ru: "и нет", en: "and no" }, start: 0, end: 0 },
-          { text: "قُوَّةَ", trans: { ru: "мощи", en: "power" }, start: 0, end: 0 },
-          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 0, end: 0 },
-          { text: "بِاللَّهِ", trans: { ru: "как у Аллаха", en: "with Allah" }, start: 0, end: 0 }
+          { text: "لَا", trans: { ru: "Нет", en: "(There is) no" }, start: 0.381, end: 0.582 },
+          { text: "حَوْلَ", trans: { ru: "силы", en: "might" }, start: 0.702, end: 1.123 },
+          { text: "وَلَا", trans: { ru: "и нет", en: "and no" }, start: 1.203, end: 1.544 },
+          { text: "قُوَّةَ", trans: { ru: "мощи", en: "power" }, start: 1.705, end: 2.286 },
+          { text: "إِلَّا", trans: { ru: "кроме", en: "except" }, start: 2.367, end: 2.848 },
+          { text: "بِاللَّهِ", trans: { ru: "как у Аллаха", en: "with Allah" }, start: 2.928, end: 3.530 }
         ],
         source: { ru: "аль-Бухари 6384; Муслим 2704", en: "al-Bukhari 6384; Muslim 2704" }
       }
